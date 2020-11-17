c515b430ed281fb06af38de3418037cd39185c2f
Cyanisyde <Mikael.jafari@gmail.com>
2020-11-17 20:42:38 +0100

    Updates

---
+++ precache-manifest.ce3a239b12f43bd39c8ee2feea4e0dda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b359f9c28e345d460098e070ebcd6a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "af82211d61a8e8ef1d28",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af82211d61a8e8ef1d28",
+    "url": "/home/static/js/main.1177ed80.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "11ae24d49dfe05d858c5fc851b998ec8",
+    "url": "/home/static/media/me.11ae24d4.png"
+  },
+  {
+    "revision": "1eeeb6547f9337e174b437598278d44b",
+    "url": "/home/static/media/resume.1eeeb654.pdf"
+  }
+]);
