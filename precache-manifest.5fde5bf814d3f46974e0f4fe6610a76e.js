cc15a89c5f9b1c485adaae254a0e0925058c0e73
Cyanisyde <Mikael.jafari@gmail.com>
2021-01-31 03:34:04 +0100

    Updates

--- precache-manifest.5fde5bf814d3f46974e0f4fe6610a76e.js
+++ precache-manifest.5fde5bf814d3f46974e0f4fe6610a76e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e74ba5851118e93524c63ee5e6fa6083",
+    "revision": "e80b0d25a84ed92f1bf958384a8367fc",
     "url": "/home/index.html"
   },
   {
-    "revision": "d695705ff838a7dfecda",
+    "revision": "d7c56561271ebc3b227b",
     "url": "/home/static/css/main.956fad23.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d695705ff838a7dfecda",
-    "url": "/home/static/js/main.2673ade1.chunk.js"
+    "revision": "d7c56561271ebc3b227b",
+    "url": "/home/static/js/main.32701c63.chunk.js"
   },
   {
     "revision": "c0578b487099793a068c",
